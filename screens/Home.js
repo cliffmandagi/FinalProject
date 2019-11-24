5e0d78bb89fc8fd965614df925fc09555ec782ad
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2019-11-24 13:59:05 +0800

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,65 @@
-import React from 'react';
-import { Platform, Dimensions, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, Dimensions, View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, FlatList } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Icon from '@expo/vector-icons/Ionicons';
 
-const Home = () => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center' , paddingLeft: 15,paddingRight:15 }}>
-          <Text style={{ fontSize: 25, fontWeight : 'bold'}}> 
-                    Welcome and Greeting
-            </Text>
-          <Text >&emsp;&emsp; Fakultas Ilmu Komputer, Universitas Klabat ini dibuat untuk melayani kebutuhan informasi bagi Staff, Dosen, Mahasiswa, alumni, pengguna lulusan, dan masyarakat umum. Website ini berisikan Informasi organisasional, akademik, maupun berita-berita yang sedang dan akan terjadi di Fakultas Ilmu Komputer, Universitas KLabat. Melalui website ini semua pihak bisa mendapatkan informasi yang terpercaya dan dapat meningkatkan citra serta meningkatkan kepercayaan terhadap Fakultas Ilmu Komputer, Universitas Klabat.</Text>
-        </View>
-    )
+
+class Home extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          data: [
+            {id:1, title: "Lorem ipsum dolor",                  time:"2018-08-01 12:15 pm", image:"https://lorempixel.com/400/200/nature/6/", description:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean  ligula..."},
+            {id:2, title: "Sit amet, consectetuer",             time:"2018-08-12 12:00 pm", image:"https://lorempixel.com/400/200/nature/5/", description:"Lorem  dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula..."} ,
+            {id:3, title: "Dipiscing elit. Aenean ",            time:"2017-08-05 12:21 pm", image:"https://lorempixel.com/400/200/nature/4/", description:"Lorem ipsum dolor sit , consectetuer  elit. Aenean commodo ligula..."}, 
+            {id:4, title: "Commodo ligula eget dolor.",         time:"2015-08-12 12:00 pm", image:"https://lorempixel.com/400/200/nature/6/", description:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula..."}, 
+            {id:5, title: "Aenean massa. Cum sociis",           time:"2013-06-12 12:11 pm", image:"https://lorempixel.com/400/200/sports/1/", description:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  commodo ligula..."}, 
+            {id:6, title: "Natoque penatibus et magnis",        time:"2018-08-12 12:56 pm", image:"https://lorempixel.com/400/200/nature/8/", description:"Lorem ipsum  sit amet, consectetuer adipiscing elit. Aenean commodo ligula..."}, 
+            {id:7, title: "Dis parturient montes, nascetur",    time:"2018-08-12 12:33 pm", image:"https://lorempixel.com/400/200/nature/1/", description:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula..."}, 
+            {id:8, title: "Ridiculus mus. Donec quam",          time:"2018-06-12 12:44 pm", image:"https://lorempixel.com/400/200/nature/3/", description:"Lorem ipsum  sit amet, consectetuer adipiscing elit.  commodo ligula..."},
+            {id:9, title: "Felis, ultricies nec, pellentesque", time:"2012-07-12 12:23 pm", image:"https://lorempixel.com/400/200/nature/4/", description:"Lorem ipsum dolor sit amet, consectetuer  elit. Aenean commodo ligula..."},
+          ]
+        };
+      }
+
+    render(){
+        return (
+            <View style={styles.container}>
+        <FlatList style={styles.list}
+          data={this.state.data}
+          keyExtractor= {(item) => item.title}
+          ItemSeparatorComponent={() => {
+            return (
+              <View style={styles.separator}/>
+            )
+          }}
+          renderItem={(post) => {
+            const item = post.item;
+            return (
+              <View style={styles.card}>
+                <Image style={styles.cardImage} source={{uri:item.image}}/>
+                <View style={styles.cardHeader}>
+                  <View>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.description}>{item.description}</Text>
+                    <View style={styles.timeContainer}>
+                      <Image style={styles.iconData} source={{uri: 'https://png.icons8.com/color/96/3498db/calendar.png'}}/>
+                      <Text style={styles.time}>{item.time}</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            )
+          }}/>
+      </View>
+        )
+    }
 }
+
+{/* <View style={{ flex: 1, justifyContent: 'center' , marginHorizontal: 15 }}>
+              <Text style={{ fontSize: 25, fontWeight : 'bold', alignSelf: 'center' }}> Welcome and Greeting</Text>
+              <Text >&emsp;&emsp; Fakultas Ilmu Komputer, Universitas Klabat ini dibuat untuk melayani kebutuhan informasi bagi Staff, Dosen, Mahasiswa, alumni, pengguna lulusan, dan masyarakat umum. Website ini berisikan Informasi organisasional, akademik, maupun berita-berita yang sedang dan akan terjadi di Fakultas Ilmu Komputer, Universitas KLabat. Melalui website ini semua pihak bisa mendapatkan informasi yang terpercaya dan dapat meningkatkan citra serta meningkatkan kepercayaan terhadap Fakultas Ilmu Komputer, Universitas Klabat.</Text>
+            </View> */}
  
 const StackNavigator =  createStackNavigator({
     Home: Home
@@ -38,3 +85,106 @@ const StackNavigator =  createStackNavigator({
 });
 
 export default createAppContainer(StackNavigator);
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+    },
+    list: {
+      paddingHorizontal: 17,
+      backgroundColor:"#E6E6E6",
+    },
+    separator: {
+      marginTop: 10,
+    },
+    /******** card **************/
+    card:{
+      shadowColor: '#00000021',
+      shadowOffset: {
+        width: 2
+      },
+      shadowOpacity: 0.5,
+      shadowRadius: 4,
+      marginVertical: 8,
+      backgroundColor:"white"
+    },
+    cardHeader: {
+      paddingVertical: 17,
+      paddingHorizontal: 16,
+      borderTopLeftRadius: 1,
+      borderTopRightRadius: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    cardContent: {
+      paddingVertical: 12.5,
+      paddingHorizontal: 16,
+    },
+    cardFooter:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingTop: 12.5,
+      paddingBottom: 25,
+      paddingHorizontal: 16,
+      borderBottomLeftRadius: 1,
+      borderBottomRightRadius: 1,
+      backgroundColor:"#EEEEEE",
+    },
+    cardImage:{
+      flex: 1,
+      height: 150,
+      width: null,
+    },
+    /******** card components **************/
+    title:{
+      fontSize:18,
+      flex:1,
+    }, 
+    description:{
+      fontSize:15,
+      color:"#888",
+      flex:1,
+      marginTop:5,
+      marginBottom:5,
+    },
+    time:{
+      fontSize:13,
+      color: "#808080",
+      marginTop: 5
+    },
+    icon: {
+      width:25,
+      height:25,
+    },
+    iconData:{
+      width:15,
+      height:15,
+      marginTop:5,
+      marginRight:5
+    },
+    timeContainer:{
+      flexDirection:'row'
+    },
+    /******** social bar ******************/
+    socialBarContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'row',
+      flex: 1
+    },
+    socialBarSection: {
+      justifyContent: 'center',
+      flexDirection: 'row',
+      flex: 1,
+    },
+    socialBarlabel: {
+      marginLeft: 8,
+      alignSelf: 'flex-end',
+      justifyContent: 'center',
+    },
+    socialBarButton:{
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
+  });   
